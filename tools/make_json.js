64e2198f5e913b02862a7d29eb02f6cc1ff33e3e
John Rees <john@bitsushi.com>
2020-05-14 11:56:35 +0100

    update data outputs

--- tools/make_json.js
+++ tools/make_json.js
@@ -1,39 +1,88 @@
+const snakeCase = require("lodash/snakeCase");
 const graph = require("../data/southwark.json");
-
-const path = "../outputs/permitted_development";
-
-const allResponses = require(`${path}/responses_and_auto_answered.json`);
-const humanResponseIds = require(`${path}/human_responses.json`);
-
 const fs = require("fs");
+const subHours = require("date-fns/subHours");
+const addSeconds = require("date-fns/addSeconds");
+const dirPath = "./outputs";
+const { v4 } = require("uuid");
 
-function isNumeric(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
+const snakeify = (ob) =>
+  Object.entries(ob).reduce((acc, [k, v]) => {
+    if (!k.startsWith("$")) acc[snakeCase(k)] = v;
+    return acc;
+  }, {});
 
-const data = allResponses.reduce((acc, curr) => {
-  const { src } = graph.edges.find(({ tgt }) => tgt === curr.id);
-
-  const ob = {
-    id: src,
-    ...graph.nodes[src],
-    response: {
-      ...curr,
-      humanResponse: humanResponseIds.includes(curr.id),
-    },
-  };
-
-  const { val } = ob.response;
-
-  if (val === "true") ob.response.val = true;
-  else if (val === "false") ob.response.val = false;
-  else if (isNumeric(val)) ob.response.val = Number(val);
-
-  acc.push(ob);
-
-  return acc;
-}, []);
-
-fs.writeFile(`${path}/data.json`, JSON.stringify(data, null, 2), (err) => {
+fs.readdir(dirPath, (err, directories) => {
   if (err) throw err;
+
+  directories
+    .filter((d) => !d.startsWith("."))
+    .forEach((directory) => {
+      const path = `${dirPath}/${directory}`;
+      const relPath = `.${path}`;
+
+      const allResponses = require(`${relPath}/responses_and_auto_answered.json`);
+      const humanResponseIds = require(`${relPath}/human_responses.json`);
+
+      function isNumeric(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+      }
+
+      let count = Math.random() * 1000;
+
+      const date = subHours(new Date(), Math.random() * 24);
+
+      const flow = allResponses.reduce((acc, curr) => {
+        const { src } = graph.edges.find(({ tgt }) => tgt === curr.id);
+
+        const responses = graph.edges
+          .filter(({ src: id }) => id === src)
+          .map(({ tgt }) => {
+            const ob = { id: tgt, ...snakeify(graph.nodes[tgt]) };
+            const { val } = ob;
+
+            if (val === "true") ob.val = true;
+            else if (val === "false") ob.val = false;
+            else if (isNumeric(val)) ob.val = Number(val);
+
+            return ob;
+          });
+
+        count += Math.random() * 30;
+
+        const ob = {
+          id: src,
+          ...snakeify(graph.nodes[src]),
+          options: responses.map(snakeify),
+          choice: {
+            id: curr.id,
+            idx: responses.findIndex((r) => r.id === curr.id),
+            recorded_at: addSeconds(date, count).toISOString(),
+            auto: !humanResponseIds.includes(curr.id),
+            // ...snakeify(curr),
+          },
+        };
+
+        acc.push(ob);
+
+        return acc;
+      }, []);
+
+      const data = {
+        id: v4(),
+        submitted_at: addSeconds(
+          date,
+          count + Math.random() * 30
+        ).toISOString(),
+        flow,
+      };
+
+      fs.writeFile(
+        `${path}/data.json`,
+        JSON.stringify(data, null, 2),
+        (err, data) => {
+          if (err) throw err;
+        }
+      );
+    });
 });
